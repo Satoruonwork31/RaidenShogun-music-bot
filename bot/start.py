26ad520b0f67a9876f011edec0ad6a7d6e241119
Satoruonwork31 <satoruatwork69@gmail.com>
2026-06-16 21:28:17 +0530

    Load plugins when starting the bot

--- bot/start.py
+++ bot/start.py
@@ -1,6 +1,8 @@
 from bot.client import app
 from bot.logger import logger
+from bot.core.loader import load_plugins
 
 def start_bot():
+    load_plugins()
     logger.info("Starting RaidenShogun Music Bot...")
     app.run()
