5fa092e7a3ffee6f52cc4d92092a06e2c97de976
Satoruonwork31 <satoruatwork69@gmail.com>
2026-06-23 23:20:51 +0530

    Add userbot client for RaidenShogunAssistant

--- bot/client.py
+++ bot/client.py
@@ -1,9 +1,16 @@
 from pyrogram import Client
-from bot.config import API_ID, API_HASH, BOT_TOKEN
+from bot.config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION
 
 app = Client(
     "RaidenShogun",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
+)
+
+userbot = Client(
+    "RaidenShogunAssistant",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=STRING_SESSION,
 )
