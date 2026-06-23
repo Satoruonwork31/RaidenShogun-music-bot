270e2ce146fae99a70f41e8ef2b78d37c1f96cb4
Satoruonwork31 <satoruatwork69@gmail.com>
2026-06-23 22:24:15 +0530

    Refactor logging message for bot startup

--- bot/start.py
+++ bot/start.py
@@ -1,8 +1,9 @@
 from bot.client import app
 from bot.logger import logger
 from bot.core.loader import load_plugins
+from bot.utils.music import music
 
 def start_bot():
     load_plugins()
-    logger.info("Starting RaidenShogun Music Bot...")
+    logger.info("Starting RaidenShogun Music Bot")
     app.run()
