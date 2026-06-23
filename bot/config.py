7280acae4286db5dd2725b151b82758caad45f86
Satoruonwork31 <satoruatwork69@gmail.com>
2026-06-23 22:37:55 +0530

    Load API credentials from environment variables
    Refactor configuration to use environment variables.

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,9 @@
-API_ID = 37501805
-API_HASH = "33d7d16f896c1371b1ea1083c09ccd06"
-BOT_TOKEN = "8998408986:AAFsWjdDDckd5PqArrrJlwPhNO4U_RoIGnE"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_ID = int(os.getenv("37501805"))
+API_HASH = os.getenv("33d7d16f896c1371b1ea1083c09ccd06")
+BOT_TOKEN = os.getenv("8998408986:AAFsWjdDDckd5PqArrrJlwPhNO4U_RoIGnE")
+STRING_SESSION = os.getenv("STRING_SESSION")
