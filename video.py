892204157cd5dc679f720a051452e74af5dd2838
Satoruonwork31 <satoruatwork69@gmail.com>
2026-06-23 18:55:47 +0530

    Delete video.py

--- video.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("video"))
-async def video_command(client, message):
-    await message.reply_text(
-        "📹 Video download system is under development."
-    )
