a1e6376bb7fe35fce3c49fa87f86a8a0533e8004
Satoruonwork31 <satoruatwork69@gmail.com>
2026-06-23 23:27:03 +0530

    Initialize userbot and music before running app

--- bot/start.py
+++ bot/start.py
@@ -1,9 +1,14 @@
-from bot.client import app
+from bot.client import app, userbot
 from bot.logger import logger
 from bot.core.loader import load_plugins
 from bot.utils.music import music
 
 def start_bot():
     load_plugins()
+
     logger.info("Starting RaidenShogun Music Bot")
+
+    userbot.start()
+    music.start()
+
     app.run()
