3f4449912fca6ec0b9e00124e1ecd22d25a4b41e
Satoruonwork31 <satoruatwork69@gmail.com>
2026-06-24 08:23:06 +0530

    Update help command with new features and emojis

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -5,17 +5,35 @@ HELP_IMAGE = "https://i.ibb.co/0yjy0Cj0/0ad5a76f9731.jpg"
 @Client.on_message(filters.command("help"))
 async def help_command(client, message):
     caption = """
-📚 RaidenShogun Music Bot Commands
+<custom_emoji id="5033104253846029290"> RaidenShogun Music Bot Commands
 
-🎵 Music
+<custom_emoji id="5334653529741076580"> Music
 • /play - Play a song
+• /vplay - Play a video in voice chat
+• /song - Search and download a song
+• /video - Search and download a video
 • /pause - Pause playback
 • /resume - Resume playback
 • /skip - Skip the current track
+• /vskip - Skip the current video
 • /stop - Stop playback
 • /queue - Show the music queue
 
-⚙️ General
+<custom_emoji id="4958900559139570572"> Moderation
+• /ban - Ban a user
+• /unban - Unban a user
+
+<custom_emoji id="5816875690183631180"> Welcome & Greetings
+• /welcome - Configure welcome settings
+• /greetings - Show greeting options
+
+<custom_emoji id="5972061723400605896"> Fun
+• /toss - Toss a coin
+
+<custom_emoji id="5350427505805238170"> Information
+• /id - Get user, group, or chat ID
+
+<custom_emoji id="5341715473882955310"> General
 • /start - Show the welcome message
 • /help - Show this help menu
 • /ping - Check if the bot is online
